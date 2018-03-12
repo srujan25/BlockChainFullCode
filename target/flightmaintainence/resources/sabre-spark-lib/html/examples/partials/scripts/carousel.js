4208144ba50182e27b05a34117b8a08b7a5d69ab
MD <sg0221562@global.ad.sabre.com>
2018-03-13 00:40:40 +0530

    target deleting

--- target/flightmaintainence/resources/sabre-spark-lib/html/examples/partials/scripts/carousel.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-  var carousels = document.querySelectorAll('.spark-carousel');
-  for (var i = 0, len = carousels.length; i < len; i++) {
-    carousels[i].test = new Spark.Carousel(carousels[i]);
-  }
-});
--- target/flightmaintainence/resources/sabre-spark-lib/html/examples/partials/scripts/menu.js
+++
@@ -1,5 +0,0 @@
-// $('.spark-menu:not(.spark-header__menu)').sparkMenu();
-var menu = document.querySelectorAll('.spark-menu:not(.spark-header__menu)');
-for(var i = 0, len = menu.length; i < len; i++) {
-  new Spark.Menu(menu[i]);
-}
